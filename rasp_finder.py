21c919d95554432a796a2fd29efa66e1c78d1e72
koromodako <koromodako@gmail.com>
2017-07-26 13:56:11 +0200

    Update rasp_finder.py

--- rasp_finder.py
+++ rasp_finder.py
@@ -1,12 +1,26 @@
 #!/usr/bin/env python3
 # -!- encoding:utf8 -!-
-
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#    file: rasp_finder.py
+#    date: 2017-07-26
+#  author: paul.dautry
+# purpose:
+#   finds a raspberry pi on my LAN using MAC addr
+# license:
+#   GPLv3
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# IMPORTS
+#-------------------------------------------------------------------------------
 from subprocess import check_output
-
+#-------------------------------------------------------------------------------
+# CONFIGURATION
+#-------------------------------------------------------------------------------
 IPV=4
 TARGET_WIFI_HWADDR='74:da:38:0a:33:60'
 TARGET_INET_HWADDR='b8:27:eb:60:cf:c9'
-
+#-------------------------------------------------------------------------------
+# FUNCTIONS
+#-------------------------------------------------------------------------------
 def run_cmd(args):
     print('run: %s' % ' '.join(args))
     return check_output(args).decode('utf-8')
@@ -40,40 +54,34 @@ You can open a SSH connection using the following command :
 > ssh -l pi %s
 """ % (ip,ip))
 
-
-# FUNCTIONS ----------------------------------------------------------
-
-# TEST PARAMS VALIDITY
-print("Searching for a raspberry-pi on your L.A.N. ...please wait...")
-print("Starting Nmap for a quick network recon...")
-
-base_addr = find_base_addr()
-
-if base_addr is None:
-    print('Failed to retrieve base address.')
-    exit(1)
-
-print('base address is : %s' % base_addr)
-nmap_discover(base_addr, '/24')
-arp_config = run_cmd(['arp', '-n'])
-
-print("Searching for raspberry-pi wifi hardware address...")
-
-ip = get_rasp_ip(arp_config, TARGET_WIFI_HWADDR)
-if ip is None:
-    print('No device found on wifi interface.')
-else:
-    print_rasp_ip(ip)
-    exit(0)
-
-print("Searching for raspberry-pi inet hardware address...")
-
-ip = get_rasp_ip(arp_config, TARGET_INET_HWADDR)
-if ip is None:
-    print('No device found on inet interface.')
-else:
-    print_rasp_ip(ip)
-    exit(0)
-
-print("No raspberry-pi found on this network !")
-exit(2)
+def main():
+    print("Searching for a raspberry-pi on your L.A.N. ...please wait...")
+    print("Starting Nmap for a quick network recon...")
+    base_addr = find_base_addr()
+    if base_addr is None:
+        print('Failed to retrieve base address.')
+        exit(1)
+    print('base address is : %s' % base_addr)
+    nmap_discover(base_addr, '/24')
+    arp_config = run_cmd(['arp', '-n'])
+    print("Searching for raspberry-pi wifi hardware address...")
+    ip = get_rasp_ip(arp_config, TARGET_WIFI_HWADDR)
+    if ip is None:
+        print('No device found on wifi interface.')
+    else:
+        print_rasp_ip(ip)
+        exit(0)
+    print("Searching for raspberry-pi inet hardware address...")
+    ip = get_rasp_ip(arp_config, TARGET_INET_HWADDR)
+    if ip is None:
+        print('No device found on inet interface.')
+    else:
+        print_rasp_ip(ip)
+        exit(0)
+    print("No raspberry-pi found on this network !")
+    exit(2)
+#-------------------------------------------------------------------------------
+# SCRIPT
+#-------------------------------------------------------------------------------
+if __name__ == '__main__':
+    main()
