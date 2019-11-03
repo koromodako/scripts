f24b157cb3bfbcc147ce4ac1c73981f71e09dc0c
koromodako <koromodako@gmail.com>
2019-11-03 19:24:20 +0100

    Improve script

--- security/mksc.py
+++ security/mksc.py
@@ -2,16 +2,20 @@
 import os
 import re
 import sys
-import enum
-import pathlib
-import argparse
-import binascii
-import subprocess
+from enum import Enum
+from logging import basicConfig, getLogger, INFO, DEBUG
+from pathlib import Path
+from argparse import ArgumentParser
+from binascii import unhexlify
+from subprocess import check_output
 
-DEBUG = False
+basicConfig(format='(%(name)s)[%(levelname)7s]: %(message)s', level=DEBUG)
+app_log = getLogger('mksc')
+app_log.setLevel(INFO)
+
 HEX_RE = re.compile(r'^[0-9a-f]{2}$')
 
-class OutputFormat(enum.Enum):
+class OutputFormat(Enum):
     Raw = 'raw'
     PrintF = 'printf'
     Python = 'python'
@@ -33,35 +37,39 @@ OUTPUT_FMT_MAP = {
     OutputFormat.Python: python_out,
 }
 
-def pdbg(msg):
-    if DEBUG:
-        sys.stderr.write(msg+'\n')
-
 def run_cmd(cmd, **kwargs):
     pcmd = ' '.join(cmd)
-    pdbg(f"running: {pcmd}")
-    return subprocess.check_output(cmd, **kwargs)
+    app_log.debug(f"running: {pcmd}")
+    return check_output(cmd, **kwargs)
 
-def generate(asm_src, bin_fmt):
-    tmp = pathlib.Path(f'/tmp/mksc-{os.urandom(8).hex()}.o')
-    run_cmd(['nasm', '-f', bin_fmt, '-o', str(tmp), str(asm_src)])
-    dat = run_cmd(['objdump', '-d', str(tmp)])
-    tmp.unlink()
+def generate(dist_dir, asm_path, bin_fmt):
+    dist_dir.mkdir(parents=True, exist_ok=True)
+    obj_path = dist_dir.joinpath(f'{asm_path.stem}.obj')
+    bin_path = dist_dir.joinpath(f'{asm_path.stem}.bin')
+    hex_path = dist_dir.joinpath(f'{asm_path.stem}.hex')
+    app_log.info("creating object from assembly...")
+    run_cmd(['nasm', '-f', bin_fmt, '-o', str(obj_path), str(asm_path)])
+    app_log.info("creating executable from object...")
+    run_cmd(['ld', '-o', str(bin_path), str(obj_path)])
+    app_log.info("creating objdump from object...")
+    dat = run_cmd(['objdump', '-d', str(obj_path)])
+    hex_path.write_bytes(dat)
     return dat
 
 def extract(objdump):
+    app_log.info("extracting shellcode from objdump...")
     hstr = ''
-    for l in objdump.split('\n'):
-        l = l.replace('\t', ' ').strip()
-        if not l:
+    for line in objdump.split('\n'):
+        line = line.replace('\t', ' ').strip()
+        if not line:
             continue
-        parts = [e for e in filter(lambda x: HEX_RE.match(x), l.split(' '))]
+        parts = [elt for elt in filter(lambda x: HEX_RE.match(x), line.split(' '))]
         hstr += ''.join(parts)
-    return bytearray(binascii.unhexlify(hstr))
+    return bytearray(unhexlify(hstr))
 
 def parse_args():
-    p = argparse.ArgumentParser(description="Making shellcode creation easy!")
-    p.add_argument('asm_src', type=pathlib.Path, help="ASM source file")
+    p = ArgumentParser(description="Making shellcode creation easy!")
+    p.add_argument('asm_path', type=Path, help="ASM source file")
     p.add_argument('--debug', action='store_true', help="Increase script verbosity")
     p.add_argument('--format',
                    choices=[fmt.value for fmt in OutputFormat],
@@ -70,26 +78,38 @@ def parse_args():
                    help="Output format")
     p.add_argument('--bin-fmt', default='elf64', help="Binary format to produce")
     p.add_argument('--pad-byte', type=int, default=0x90, help="Padding byte to add at the end if necessary")
+    p.add_argument('--dist-dir', default=Path('mksc.dist'), type=Path, help="Distribution directory")
     p.add_argument('--expected-size', type=int, default=-1, help="Payload size required at the end")
-    p.add_argument('--extra-suffix', help="Payload raw suffix")
-    p.add_argument('--extra-prefix', help="Payload raw prefix")
+    p.add_argument('--hex-suffix', help="Payload hex-encoded suffix")
+    p.add_argument('--hex-prefix', help="Payload hex-encoded prefix")
     return p.parse_args()
 
-if __name__ == '__main__':
-    args = parse_args()
-    DEBUG = args.debug
-    dat = generate(args.asm_src, args.bin_fmt)
-    pdbg(dat.decode())
+def app(args):
+    # generate required elements from assembly source file
+    dat = generate(args.dist_dir,
+                   args.asm_path,
+                   args.bin_fmt)
+    # extract shellcode from objdump output
     dat = extract(dat.decode())
+    # perform padding if required
     pad_size = args.expected_size - len(dat)
     if pad_size < 0 and args.expected_size > 0:
-        pdbg(f"warning: shellcode size ({len(dat)}) exceeds expected size")
+        app_log.warning(f"warning: shellcode size ({len(dat)}) exceeds expected size")
     if pad_size > 0:
         dat += pad_size * bytearray([args.pad_byte])
-    if args.extra_suffix:
-        dat += args.extra_suffix.encode()
-    if args.extra_prefix:
-        dat = args.extra_prefix.encode() + dat
+    # append hex-encoded suffix
+    if args.hex_suffix:
+        dat += unhexlify(args.hex_suffix)
+    # prepend hex-encoded prefix
+    if args.hex_prefix:
+        dat = unhexlify(args.hex_prefix) + dat
     dat = bytes(dat)
+    app_log.info("printing payload:")
     OUTPUT_FMT_MAP[args.format](dat)
 
+if __name__ == '__main__':
+    # parse command line arguments
+    args = parse_args()
+    if args.debug:
+        app_log.setLevel(DEBUG)
+    app(args)
